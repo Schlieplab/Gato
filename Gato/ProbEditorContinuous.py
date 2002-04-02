582015b62e6727b2d93eb2640f350fe9601ced99
Alexander Schliep <alexander@schlieplab.org>
2002-04-02 10:59:30 +0000

    Replaced _COPYRIGHT_ with proper COPYRIGHT notice in preparation for move to sourceforge

--- Gato/ProbEditorContinuous.py
+++ Gato/ProbEditorContinuous.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 ################################################################################
 #
-#       This file is part of Gato (Combinatorial Algorithm Toolbox) 
+#       This file is part of Gato (Graph Algorithm Toolbox) 
 #       version _VERSION_ from _BUILDDATE_. You can find more information at 
 #       http://www.zpr.uni-koeln.de/~gato
 #
 #	file:   ProbEditorContinuous.py
 #	author: Achim Gaedke (achim.gaedke@zpr.uni-koeln.de)
 #
-#       _COPYRIGHT_
+#       Copyright (C) 1998-2002, Alexander Schliep, Winfried Hochstaettler and 
+#       ZAIK/ZPR, Universitaet zu Koeln
+#                                   
+#       Contact: schliep@zpr.uni-koeln.de, wh@zpr.uni-koeln.de             
+#
+#       Information: http://gato.sf.net
+#
+#       This library is free software; you can redistribute it and/or
+#       modify it under the terms of the GNU Library General Public
+#       License as published by the Free Software Foundation; either
+#       version 2 of the License, or (at your option) any later version.
+#
+#       This library is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#       Library General Public License for more details.
+#
+#       You should have received a copy of the GNU Library General Public
+#       License along with this library; if not, write to the Free
+#       Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
 #
 #       This file is version $Revision$ 
 #                       from $Date$
