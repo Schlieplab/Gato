4f8bab927d98870469fdfc4d6b556ccc6097ff18
Alexander Schliep <alexander@schlieplab.org>
2011-03-04 16:29:37 +0000

    Updated instructions

--- Gato/WWW/copy-to-sf.sh
+++ Gato/WWW/copy-to-sf.sh
@@ -3,7 +3,12 @@
 # Update gato.sf.net website at Sourceforge from a checked out version
 # from CVS
 #
+#
+#
+#
+#
+# Files are at /home/project-web/gato/htdocs after shell login
 
-rsync -r -R --cvs-exclude . gato.sf.net:/home/groups/g/ga/gato/htdocs
-ssh gato.sf.net "find /home/groups/g/ga/gato/htdocs -type f -exec chmod 664 {} \;"
-ssh gato.sf.net "find /home/groups/g/ga/gato/htdocs -type d -exec chmod 775 {} \;"
+rsync -aiv -r -R --cvs-exclude . schliep,gato@web.sourceforge.net:htdocs/
+#ssh gato.sf.net "find /home/groups/g/ga/gato/htdocs -type f -exec chmod 664 {} \;"
+#ssh gato.sf.net "find /home/groups/g/ga/gato/htdocs -type d -exec chmod 775 {} \;"
