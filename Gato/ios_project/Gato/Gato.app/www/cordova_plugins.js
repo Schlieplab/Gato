5f1f76c08df17cb63f29d5b437efc7be52f6872a
Scott Merkling <sbmerkling@gmail.com>
2015-06-10 20:57:04 +0000

    Added working version of app

---
+++ Gato/ios_project/Gato/Gato.app/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/hu.dpal.phonegap.plugins.SpinnerDialog/www/spinner.js",
+        "id": "hu.dpal.phonegap.plugins.SpinnerDialog.SpinnerDialog",
+        "merges": [
+            "window.plugins.spinnerDialog"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "hu.dpal.phonegap.plugins.SpinnerDialog": "1.3.1"
+}
+// BOTTOM OF METADATA
+});
